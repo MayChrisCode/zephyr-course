8d13eea5e3403d766b598d5b8ea99906170aaa26
MayChrisCode <christophmayerhofer@gmx.de>
2026-09-25 14:47:25 +0000

    completing assignment 04

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -7,8 +7,8 @@
 
 #define SLEEP_TIME_MS 1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+/* The devicetree node identifier for the "app_led" alias. */
+#define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -19,7 +19,7 @@ int main(void)
     bool led_state = true;
 
     if (!gpio_is_ready_dt(&led)) return 0;
-    printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
 
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
@@ -28,8 +28,8 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
-        printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+;
     }
     return 0;
 }
